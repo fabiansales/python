802651657349645e8a60ce367f9fbb20f351406a
Fabian Sales <fabiansales@gmail.com>
2023-07-18 16:00:56 +0200

    Fix capital letters (upper and lower case) and accents

--- lead-year/year.py
+++ lead-year/year.py
@@ -1,15 +1,13 @@
 year = int(input("Ingrese un año: "))
 
 if year < 1582:
-   print("Ingrese un año mayor a 1582 para que este dentro del calendario Gregoriano")
+   print("\nIngrese un año mayor a 1582 para que este dentro del calendario Gregoriano\nEnter a year greater than 1582 to be within the Gregorian Calendar\n")
 else:
     if year % 4 != 0:
-      # año comun
-      print("Año comun: ",)
+      print("Año Común: ",year)
     elif year % 100 != 0:
-       # año bisiesto
-       print("Año bisiesto: ",year)
+       print("Año Bisiesto: ",year)
     elif year % 400 != 0:
-       print("Año comun: ",year)
+       print("Año Común: ",year)
     else:       
-       print("Año bisiesto: ",year )
+       print("Año Bisiesto: ",year )
