eb55bcd5deab998631a1f3223dce1b80e8fa42fa
Fabian Sales <fabiansales@gmail.com>
2023-07-18 18:32:05 +0200

    Added a details about if-elif-else

--- leap-year/year.py
+++ leap-year/year.py
@@ -11,3 +11,25 @@ else:
        print("Año Común: ",year)
     else:       
        print("Año Bisiesto: ",year )
+'''
+x = 10
+ 
+if x == 10: # True
+    print("x == 10")
+ 
+if x > 15: # False
+    print("x > 15")
+ 
+elif x > 10: # False
+    print("x > 10")
+ 
+elif x > 5: # True
+    print("x > 5")
+ 
+else:
+    print("else no será ejecutado")
+
+Si la condición para if es False, el programa verifica las condiciones de los bloques elif posteriores: 
+el primer elif que sea True es el que se ejecuta. Si todas las condiciones son False, se ejecutará el bloque else.    
+
+'''
